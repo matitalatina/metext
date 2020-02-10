584a4d59dd36f96b8d543a6a73892b783d07a853
Mattia Natali <mattia.natali@pirelli.com>
2020-02-10 20:15:46 +0100

    Added sorting and select all feature

--- lib/main.dart
+++ lib/main.dart
@@ -117,3 +117,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
--- lib/pages/show_text_page.dart
+++ lib/pages/show_text_page.dart
@@ -4,7 +4,7 @@ import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class ShowTextPage extends StatelessWidget {
+class ShowTextPage extends StatefulWidget {
   ShowTextPage({Key key, @required this.visionText, @required this.image})
       : super(key: key);
 
@@ -12,25 +12,110 @@ class ShowTextPage extends StatelessWidget {
   final VisionText visionText;
 
   @override
+  _ShowTextPageState createState() => _ShowTextPageState();
+}
+
+class _ShowTextPageState extends State<ShowTextPage> {
+  List<ExtractedText> extractedTexts;
+
+  @override
   Widget build(BuildContext context) {
+    this.extractedTexts = this.extractedTexts ??
+        this
+            .widget
+            .visionText
+            .blocks
+            .map((b) => ExtractedText(selected: false, text: b.text))
+            .toList();
+    final selectedCount = this.extractedTexts
+        .where((t) => t.selected)
+        .length;
+    final hasMoreSelected = selectedCount > this.extractedTexts.length / 2;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Extracted text"),
+        title: Text("$selectedCount selected blocks"),
+        actions: [
+          hasMoreSelected
+              ? IconButton(
+            icon: Icon(Icons.check_box),
+            onPressed: () => onSelectAll(false),
+            tooltip: "Unselect all",
+          )
+              : IconButton(
+            icon: Icon(Icons.check_box_outline_blank),
+            onPressed: () => onSelectAll(true),
+            tooltip: "Select all",
+          )
+        ],
       ),
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: this.visionText.blocks.length,
-          itemBuilder: (context, index) => ListTile(
-            title: SelectableText(this.visionText.blocks[index].text),
-            trailing: IconButton(icon: Icon(Icons.content_copy), onPressed: () {
-              Clipboard.setData(new ClipboardData(text: this.visionText.blocks[index].text));
-              Scaffold.of(context).showSnackBar(
-                  SnackBar(content: Text("Copied!"), duration: Duration(seconds: 1),)
-              );
-            },),
-          ),
-        ),
-      ),
+          child: ReorderableListView(
+            onReorder: (oldIndex, newIndex) {
+              setState(() {
+                if (oldIndex > newIndex) {
+                  final swappedText = this.extractedTexts.removeAt(oldIndex);
+                  this.extractedTexts.insert(newIndex, swappedText);
+                }
+                else {
+                  final swappedText = this.extractedTexts.removeAt(oldIndex);
+                  this.extractedTexts.insert(--newIndex, swappedText);
+                }
+              });
+            } ,
+              children:
+              this.extractedTexts
+                  .map((t) {
+                return ListTile(
+                  key: Key(t.text),
+                  onTap: () => onToggleText(t),
+                  title: Text(t.text),
+                  leading: Icon(t.selected
+                      ? Icons.check_box
+                      : Icons.check_box_outline_blank),
+                  trailing: Icon(Icons.drag_handle),
+                  selected: t.selected
+                  ,
+                );
+              }
+              ).toList()
+          )),
+      floatingActionButton: selectedCount != 0
+          ? Builder(
+        builder: (BuildContext context) =>
+            FloatingActionButton(
+              child: Icon(Icons.content_copy),
+              tooltip: "Copy selected blocks",
+              onPressed: () {
+                Clipboard.setData(new ClipboardData(
+                    text: this.extractedTexts.map((t) => t.text).join("\n")));
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text("Copied!"),
+                  duration: Duration(seconds: 1),
+                ));
+              },
+            ),
+      )
+          : null,
     );
   }
+
+  onToggleText(ExtractedText index) {
+    setState(() {
+      index.selected =
+      !index.selected;
+    });
+  }
+
+  onSelectAll(bool selected) {
+    setState(() {
+      this.extractedTexts.forEach((t) => t.selected = selected);
+    });
+  }
+}
+
+class ExtractedText {
+  bool selected;
+  String text;
+
+  ExtractedText({this.selected, this.text});
 }

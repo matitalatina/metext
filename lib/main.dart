a5a80d59bd34163d2731e0ee0e86b1771e5a1689
Mattia Natali <matitalatina@gmail.com>
2020-04-26 11:32:22 +0200

    Updated to v1.0.3

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: appName,
-      debugShowCheckedModeBanner: false,
+//      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
         fontFamily: 'Montserrat',
